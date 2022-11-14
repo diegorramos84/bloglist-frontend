0f6042c7779be70d6a84e04ebc470a3df22172ec
Diego Ramos <diegorramos84@gmail.com>
2022-11-14 16:18:26 +0000

    updated react to v18 to work with the code from the model

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+// had to update react to v18 - npm install react-dom@latest react@latest
