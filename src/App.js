39c5e090d4830c4e36ea1e440fb2d39cb860d72b
Diego Ramos <diegorramos84@gmail.com>
2022-11-14 18:35:46 +0000

    add notification and conditional rendering if logged in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginservices from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState()
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [user, setUser] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -14,38 +17,69 @@ const App = () => {
     )
   }, [])
 
-  const handleLogin =(event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('logging in with', username, password)
+
+    try {
+      const user = await loginservices.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000);
+    }
   }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+        type="text"
+        name="Username"
+        value={username}
+        onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+        type="text"
+        name="Password"
+        value={password}
+        onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+    <button type="submit">login</button>
+  </form>
+  )
+
+  const allBlogs = () => (
+    blogs.map(blog =>
+      <Blog key={blog.id} blog={blog} />
+    )
+  )
 
   return (
     <div>
       <h2>blogs</h2>
 
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-          type="text"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-          type="text"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
+    <Notification message={errorMessage} />
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+    {user === null ?
+      loginForm() : // if user is null show login form
+      <div>
+        {console.log(user)}
+        <p>{user.name} is logged in</p>
+        {allBlogs()}
+      </div>
+    }
+
     </div>
   )
 }
