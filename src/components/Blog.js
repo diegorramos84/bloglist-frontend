65e0379a48b3093d5657383c26780525cfbdee8f
Diego Ramos <diegorramos84@gmail.com>
2022-12-14 10:47:17 +0000

    add view/hide functionability to blogs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        <p>{blog.title} <button onClick={toggleVisibility}>view</button></p>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.title} <button onClick={toggleVisibility}>hide</button></p>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes}</p>
+        <p>{blog.author}</p>
+
+      </div>
+    </div>
+)}
 
 export default Blog
