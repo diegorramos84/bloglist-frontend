75367971691bebdcc7771ebfb30b28598519d83c
Diego Ramos <diegorramos84@gmail.com>
2023-01-04 19:27:00 +0000

    5.17 step1 - install cypress and run first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
   "env": {
       "browser": true,
       "es6": true,
-      "jest/globals": true
+      "jest/globals": true,
+      "cypress/globals": true
   },
   "extends": [
       "eslint:recommended",
